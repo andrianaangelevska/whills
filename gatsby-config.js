aad960cb4446759db0937e5efa2b8e6a83b652b5
Rohit <rohit@novvuminc.com>
2019-07-11 10:52:56 -0700

    updated theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   __experimentalThemes: [
     {
-      resolve: "@novvum/gatsby-theme-wiki",
-    },
+      resolve: "@novvum/gatsby-theme-wiki"
+    }
   ],
   siteMetadata: {
     title: `Novvum Whills`,
     author: `Novvum`,
-    description: `An extension of the gatsby starter blog, with support for MDX`,
+    description: `Welcome to the Novvum Whills.`,
     siteUrl: `https://whills.novvum.io/`,
     social: {
-      twitter: `novvumio`,
-    },
-  },
-}
+      twitter: `novvumio`
+    }
+  }
+};
